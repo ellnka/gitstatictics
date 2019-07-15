2d74dd9d3249dc26eff8dc357c489c4e90a6e294
Elena Y <alenkasmail@gmail.com>
2019-07-16 00:37:23 +0300

    init build

---
+++ build/precache-manifest.24a424ea58d17fa9c7a3e411450b2f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fba8434f859a68f292f06d747c61946",
+    "url": "/ellnka/gitstats/index.html"
+  },
+  {
+    "revision": "8b7eb11cbbe8f4fb4418",
+    "url": "/ellnka/gitstats/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ae872e292ef88cfd5a41",
+    "url": "/ellnka/gitstats/static/css/main.6f974788.chunk.css"
+  },
+  {
+    "revision": "8b7eb11cbbe8f4fb4418",
+    "url": "/ellnka/gitstats/static/js/2.20c8a0cd.chunk.js"
+  },
+  {
+    "revision": "ae872e292ef88cfd5a41",
+    "url": "/ellnka/gitstats/static/js/main.5ae2dfcc.chunk.js"
+  },
+  {
+    "revision": "a86e76fca747beb7cfd2",
+    "url": "/ellnka/gitstats/static/js/runtime~main.89062d7c.js"
+  }
+]);
