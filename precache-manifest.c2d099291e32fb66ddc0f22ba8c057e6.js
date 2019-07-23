1cecf5112cc624d1180636d53ca4519f0ac4c691
Elena Y <alenkasmail@gmail.com>
2019-07-24 01:32:38 +0300

    git stats with routes

---
+++ precache-manifest.c2d099291e32fb66ddc0f22ba8c057e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2646595f28da3618ca7c4e25507cde24",
+    "url": "/gitstats/index.html"
+  },
+  {
+    "revision": "0e3b4b798521be81d411",
+    "url": "/gitstats/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4d09437f4b159e610450",
+    "url": "/gitstats/static/css/main.f55d9146.chunk.css"
+  },
+  {
+    "revision": "0e3b4b798521be81d411",
+    "url": "/gitstats/static/js/2.cdb988a6.chunk.js"
+  },
+  {
+    "revision": "4d09437f4b159e610450",
+    "url": "/gitstats/static/js/main.dc48520f.chunk.js"
+  },
+  {
+    "revision": "00d277ec69ae644b87db",
+    "url": "/gitstats/static/js/runtime~main.c897b6d6.js"
+  }
+]);
